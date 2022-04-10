4330f9eba40d4c74d143ccd702f794cdbe9f0dac
stampixel <stampixel.t@gmail.com>
2022-04-10 19:15:17 -0400

    Updated server.js

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const server = http.createServer(app);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-const PORT = 3001 || process.env.PORT
+const PORT = 3002 || process.env.PORT
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
